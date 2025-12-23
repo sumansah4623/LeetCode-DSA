d2d18675885fc8f6c67ed5d935bc21dec88ad1f1
suman <sumansah4623@gmail.com>
2025-12-23 23:31:45 +0530

    Sum of natural number gfg Recursive solution

---
+++ Recursion/Sum-of-natural-number-gfg.cpp
@@ -0,0 +1,42 @@
+//Approach 1 : iterative approach
+//time complexity : O(n)
+//space complexity : O(1)
+// class Solution 
+// {
+//   public:
+//     int findSum(int n) 
+//     {
+//         int sum = 0;
+//         for(int i = 1; i<=n; i++)
+//         {
+//             sum = sum + i;
+//         }
+        
+//         return sum;
+//     }
+// };
+
+
+
+
+// Approach 2 : Recursive approach
+// Time Complexity : O(n)
+// Space Complexity : O(n)     due to recursive call stack
+class Solution 
+{
+  public:
+    int findSum(int n) 
+    {
+        //base case
+        if(n == 1)
+        {
+            return 1;
+        }
+        
+        //Recursive Function
+        int smallProblem = findSum(n-1);
+        int bigProblem = n + smallProblem;
+        
+        return bigProblem;    //return solution of big problem
+    }
+};
